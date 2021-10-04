5c315a4970f2c373e83db28958637aa5f16ea02e
ankit <ankitforstudy@gmail.com>
2021-10-04 19:59:14 +0530

    added security specific to HttpMethod Type

--- src/main/java/com/example/springsecuritybasicauthauthorities/config/CustomWebSecurityConfigurerAdaptor.java
+++ src/main/java/com/example/springsecuritybasicauthauthorities/config/CustomWebSecurityConfigurerAdaptor.java
@@ -1,6 +1,7 @@
 package com.example.springsecuritybasicauthauthorities.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -11,7 +12,9 @@ public class CustomWebSecurityConfigurerAdaptor extends WebSecurityConfigurerAda
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic();
         http.authorizeRequests().mvcMatchers("**/post/**").hasAuthority("write").and()
+                .authorizeRequests().mvcMatchers(HttpMethod.POST, "**/post/**").hasAuthority("write").and()
                 .authorizeRequests().mvcMatchers("**/get/**").hasAuthority("read").and()
+                .authorizeRequests().mvcMatchers(HttpMethod.GET, "**/get/**").hasAuthority("read").and()
                 .authorizeRequests().mvcMatchers("**/delete/**").access("hasAuthority('delete')");
     }
 
